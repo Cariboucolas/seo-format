203a93d2739c8d6727558c792cd4edd669bef635
Colas Durcy <cariboucolas@gmail.com>
2025-07-30 16:26:56 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export const App = () => {
+	return (
+		<div className="App">
+			<h1 className="m-auto px-4 my-4 max-w-lg">Welcome to the App</h1>
+			<p>This is a simple React application.</p>
+		</div>
+	);
+};
